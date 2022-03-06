06f1557c43d3f0b9befcaa8ac5235a60afb13dc0
M1chae10s <m1chae10s@mail.ru>
2022-03-06 12:37:02 +0400

    lesson01

---
+++ lesson01/script.js
@@ -0,0 +1,8 @@
+let money = null;
+let income = null;
+let addExpenses = null;
+let deposit = null;
+let mission = null;
+let period = null;
+alert("самсынг вронг");
+console.log("носынг вронг, евресинк олрайт ;)");
