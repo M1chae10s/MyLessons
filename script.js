b8b9fe50392c900964a30c6d8ff899a4716fd853
M1chae10s <m1chae10s@mail.ru>
2022-03-17 09:35:12 +0400

    Lesson02

---
+++ script.js
@@ -0,0 +1,19 @@
+let money = 99999;
+let income = 'Part-time job';
+let addExpenses = 'bill, transport, food';
+let deposit = true;
+let mission = 999999999999999;
+let period = 1;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log("Период равен "+period+" месяцев");
+console.log("Цель заработать "+mission+" рублей");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(", "));
+let budgetDay = money;
+console.log(budgetDay);
+
